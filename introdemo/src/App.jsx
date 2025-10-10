637bdb8701df1019f4bfe7afa6d206375799fda4
ductrl <lytriducgp@gmail.com>
2025-10-10 17:33:28 -0400

    A bunch of stuff in part 1 I haven't committed

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,54 +1,17 @@
-import { use, useState } from "react";
+import { useState } from 'react';
+import Note from './components/Note';
 
-const History = (props) => {
-  if (props.allClicks.length == 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    );
-  }
+const App = ({ notes  }) => {
   return (
     <div>
-      button press history: {props.allClicks.join(', ')}
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+        < Note key={note.id} note={note}/>
+      )}
+      </ul>
     </div>
   );
-}
-
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>;
-
-const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-  const [total, setTotal] = useState(0);
-
-  const handleLeftClick = () => { 
-    setAll(allClicks.concat("L"));
-    const updatedLeft = left + 1;
-    setLeft(updatedLeft);
-    setTotal(updatedLeft + right);
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-    setTotal(left + updatedRight);
-  };
-
-  return (
-    <>
-      {left}
-      <Button onClick={handleLeftClick} text={"left"}/>
-      <br />
-      {right}
-      <Button onClick={handleRightClick} text={"right"}/>
-      <br />
-      {total}
-      <History allClicks={allClicks}/>
-    </>
-  );
-}
+};
 
 export default App;
