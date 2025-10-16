6867c580311574792509e54cd774668616690ba0
ductrl <lytriducgp@gmail.com>
2025-10-15 21:56:07 -0400

    Completed part 2d

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -1,24 +1,35 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import Note from './components/Note';
+import noteService from './services/notes';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('a new note...');
   const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
-    console.log('effect');
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled');
-        setNotes(response.data);
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialResponse => {
+        setNotes(initialResponse);
       })
-  };
+  }, []);
 
-  useEffect(hook, []);
-  console.log('render', notes.length, 'notes');
+  const toggleImportance = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find(n => n.id === id);
+    const changedNote = {...note, important: !note.important};
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id === id ? returnedNote : note));
+      })
+      .catch(error => {
+        alert(`the note '${note.content}' was already deleted from the server`);
+        setNotes(notes.filter(n => n.id !== id));
+      })
+  }
 
   const addNote = (event) => {
     event.preventDefault();
@@ -28,12 +39,15 @@ const App = () => {
       id: `${notes.length + 1}`
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote('');
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote));
+        setNewNote('');
+      })
   }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value);
     setNewNote(event.target.value);
   }
 
@@ -44,7 +58,7 @@ const App = () => {
       <h1>Notes</h1>
       <ul>
         {notesToShow.map(note => 
-        < Note key={note.id} note={note}/>
+        < Note key={note.id} note={note} toggleImportance={() => toggleImportance(note.id)}/>
       )}
       </ul>
       <div>
