369147886edf464b0c5e13502b9e76f84d341ee9
ductrl <lytriducgp@gmail.com>
2025-12-14 21:51:20 -0500

    Added frontend

--- exercises/phonebook_backend/index.js
+++ exercises/phonebook_backend/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('dist'));
 
 morgan.token('person', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms :person'));
