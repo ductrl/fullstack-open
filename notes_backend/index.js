94c7e3d899f6214aaec6ec7d68dd7acf2d6a0390
ductrl <lytriducgp@gmail.com>
2025-12-16 23:23:39 -0500

    Added mongoose validation

--- notes_backend/index.js
+++ notes_backend/index.js
@@ -6,9 +6,11 @@ const Note = require('./models/note');
 const errorHandler = (error, request, response, next) => {
     console.log(error.message);
     
-    if (error.name == 'CastError')
-        return response.status(400).send({ error: 'malformatted id' });
-
+    if (error.name === 'CastError')
+        return response.status(400).json({ error: 'malformatted id' });
+    if (error.name === 'ValidationError')
+        return response.status(400).json({ error: error.message });
+    
     next(error);
 }
 
@@ -44,21 +46,19 @@ app.delete('/api/notes/:id', (request, response, next) => {
         .catch(error => next(error));
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body;
-    
-    if (!body.content) {
-        return response.status(400).json({ error: 'content missing' });
-    }
 
     const note = new Note({
         content: body.content,
         important: body.important || false,
     });
 
-    note.save().then(savedNote => {
-        response.json(savedNote);
-    });
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote);
+        })
+        .catch(error => next(error));
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
