4f88c81e21df97cd5369883327932f1987787cd9
ductrl <lytriducgp@gmail.com>
2025-12-16 22:20:59 -0500

    Completed part 3c

--- notes_backend/index.js
+++ notes_backend/index.js
@@ -3,6 +3,15 @@ const express = require('express');
 const app = express();
 const Note = require('./models/note');
 
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message);
+    
+    if (error.name == 'CastError')
+        return response.status(400).send({ error: 'malformatted id' });
+
+    next(error);
+}
+
 app.use(express.json());
 app.use(express.static('dist'));
 
@@ -16,17 +25,23 @@ app.get('/api/notes', (request, response) => {
     });
 });
 
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note);
-    });
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note)
+                response.json(note);
+            else 
+                response.status(404).end();
+        })
+        .catch(error => next(error));
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id;
-    notes = notes.filter(note => note.id !== id);
-
-    response.status(204).end();
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
 })
 
 app.post('/api/notes', (request, response) => {
@@ -45,6 +60,26 @@ app.post('/api/notes', (request, response) => {
         response.json(savedNote);
     });
 });
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const { content, important } = request.body;
+
+    Note.findById(request.params.id)
+        .then(note => {
+            if (!note)
+                return response.status(404).end();
+
+            note.content = content;
+            note.important = important;
+
+            return note.save().then(updatedNote => {
+                response.json(updatedNote);
+            });
+        })
+        .catch(error => next(error));
+});
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
