496e8eced4657963344994aa8fe6dc2c8e97a8f7
ductrl <lytriducgp@gmail.com>
2025-12-29 22:04:08 +0700

    Completed exercises 4.1 and 4.2

---
+++ exercises/blog_backend/app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+const blogRouter = require('./controllers/blogs');
+
+const app = express();
+
+logger.info('Connecting to MongoDB');
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('Connected to MongoDB!');
+  })
+  .catch((error) => {
+    logger.error('Error while connecting to MongoDB: ', error.message);
+  });
+
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use('/api/blogs/', blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
